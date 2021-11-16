2f7af4c9d00b8ea55c42e235b18954d6db556cf5
Seongjun Cho <seongjch@c5r1s1.42seoul.kr>
2021-11-16 17:26:42 +0900

    today_diner

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,11 @@
 /*   By: seongjch <seongjch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:39:03 by seongjch          #+#    #+#             */
-/*   Updated: 2021/11/16 16:23:34 by seongjch         ###   ########.fr       */
+/*   Updated: 2021/11/16 17:03:51 by seongjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char	*b_dst;
 	unsigned char	*b_src;
